cf2a83f1a8bcb1512f02a14905ce34f316102ba4
Johnny Chen <Johnnychen384@gmail.com>
2023-02-26 00:39:37 -0500

    finished all User class tests

--- tests/User.test.js
+++ tests/User.test.js
@@ -11,11 +11,38 @@ test("Test users username property", () => {
 })
 
 // test password
+test("Test users password property", () => {
+    const newUser = new User("Johnny", 123, 1)
+    expect(newUser.password).toBe(123)
+})
 
 // test age
+test("Test users age property", () => {
+    const newUser = new User("Johnny", 123, 1)
+    expect(newUser.age).toBe(1)
+})
 
 // test login
+test("Test login method for user class", () => {
+    const newUser = new User("Johnny", 123, 1)
+    const consoleSpy = jest.spyOn(console, 'log')
+    newUser.login(123)
+
+    expect(consoleSpy).toHaveBeenCalledWith("Successful login")
+})
+
+test("Test if login method fails for user class", () => {
+    const newUser = new User("Johnny", 123, 1)
+    expect(() => newUser.login(1)).toThrowError("incorrect password")
+})
 
 // test logout
+test("Test logout method for user class", () => {
+    const newUser = new User("Johnny", 123, 1)
+    const consoleSpy = jest.spyOn(console, 'log')
+    newUser.logout()
+
+    expect(consoleSpy).toHaveBeenCalledWith("user has been logged out.")
+})
 
 })
